ef2c169ff6c2533bb827677e1a3d77c3d4093ea1
SIMOHAMMED22 <simofinance22@gmail.com>
2024-03-21 23:54:19 +0000

    fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
---SQL script that ranks country origins of bands,
---ordered by the number of (non-unique) fans
-SELECT origin, SUM(nb_fans) as nb_fans
+-- SQL script that ranks country origins of bands
+-- ordered by the number:
+SELECT origin, SUM(fans) as nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
