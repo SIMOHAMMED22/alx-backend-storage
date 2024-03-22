c0e77876aac1c0fc333b1e922e146c39e9ab0285
SIMOHAMMED22 <simofinance22@gmail.com>
2024-03-22 15:56:48 +0000

    average

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,23 +1,16 @@
---SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser
---that computes and store the average weighted score for a student.
-DELIMITER //
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
+--  a SQL script that creates a stored procedure
+-- ComputeAverageWeightedScoreForUser that computes
+-- and store the average weighted score for a student.
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE total_score DECIMAL(10,2);
-    DECLARE total_weight DECIMAL(10,2);
-    DECLARE avg_weighted_score DECIMAL(10,2);
-
-    SELECT SUM(score * weight) INTO total_score, SUM(weight) INTO total_weight
-    FROM scores
-    WHERE user_id = user_id;
-
-    IF total_weight > 0 THEN
-        SET avg_weighted_score = total_score / total_weight;
-    ELSE
-        SET avg_weighted_score = 0;
-    END IF;
-
-    INSERT INTO average_weighted_scores (user_id, average_score)
-    VALUES (user_id, avg_weighted_score);
-END //
+    UPDATE users
+    SET average_score = (
+        SELECT IF(SUM(score * weight) > 0, SUM(score * weight) / SUM(weight), 0)
+        FROM corrections
+        JOIN projects ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id
+    )
+    WHERE id = user_id;
+END$$
 DELIMITER ;
