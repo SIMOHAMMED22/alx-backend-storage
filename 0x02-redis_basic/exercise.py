f72f5279c83eb476f98956b40514b0650525668b
SIMOHAMMED22 <simofinance22@gmail.com>
2024-03-28 17:20:29 +0000

    exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,34 +1,32 @@
 #!/usr/bin/env python3
-"""Main"""
-
-import redis
-import uuid
-from typing import Union, Callable, Optional, List
+"""this is module for redis"""
 from functools import wraps
+import redis
+from typing import Union, Callable, Optional, List
+import uuid
 
 
 def count_calls(method: Callable) -> Callable:
-    """count how many times methods of Cache class are called"""
+    """count calls method for redis"""
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """wrap the decorated"""
+        """wrapper method for redis"""
         key = method.__qualname__
         self._redis.incr(key)
         return method(self, *args, **kwargs)
-
     return wrapper
 
 
 def call_history(method: Callable) -> Callable:
-    """ store the history  """
+    """call history method for redis"""
     key = method.__qualname__
     inputs = key + ":inputs"
     outputs = key + ":outputs"
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """ wrapped function """
+        """wrapper method for redis"""
         self._redis.rpush(inputs, str(args))
         result = method(self, *args, **kwargs)
         self._redis.rpush(outputs, str(result))
@@ -53,39 +51,42 @@ def replay(redis_instance: redis.Redis, method: Callable) -> List[str]:
 
 
 class Cache:
-    """ class """
-
+    """main class for redis"""
     def __init__(self):
-        """ init redis """
+        """init method for redis"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ generate a random key to store data """
+        """sotre method for redis"""
         key = str(uuid.uuid4())
-        self._redis.set(key, data)
+        if isinstance(data, str):
+            self._redis.set(key, data)
+        elif isinstance(data, bytes):
+            self._redis.set(key, data)
+        elif isinstance(data, int):
+            self._redis.set(key, str(data))
+        elif isinstance(data, float):
+            self._redis.set(key, str(data))
         return key
 
-    def get(self, key: str,
-            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
-        """ convert data using cb """
-        data = self._redis.get(key)
-        if fn:
-            return fn(data)
-        return data
+    def get(self, key: str, fn: Callable = None) -> \
+            Union[str, bytes, int, float]:
+        """get method for redis"""
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is not None:
+            return fn(value)
+        else:
+            return value
 
     def get_str(self, key: str) -> str:
-        """ automatically parametrize Cache.get to str """
-        data = self._redis.get(key)
-        return data.decode("utf-8")
+        """get str method for redis"""
+        return self.get(key, lambda d: d.decode("utf-8"))
 
     def get_int(self, key: str) -> int:
-        """ automatically parametrize Cache.get to int """
-        value = self._redis.get(key)
-        try:
-            value = int(value.decode("utf-8"))
-        except Exception:
-            value = 0
-        return value
+        """get int method for redis"""
+        return self.get(key, int)
